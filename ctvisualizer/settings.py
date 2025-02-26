6f22413c37818ebc739a264e7f6ebdb12933eb64
Johnson-Jeeva <johnsonjeeva2000@gmail.com>
2025-02-26 16:25:37 -0700

    Database updated

--- ctvisualizer/settings.py
+++ ctvisualizer/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +76,16 @@ WSGI_APPLICATION = 'ctvisualizer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ctviewer_db',  
+        'USER': 'ctviewer_user',  
+        'PASSWORD': 'John@0512',  
+        'HOST': 'localhost',  
+        'PORT': '', 
     }
 }
 
